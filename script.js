c8ab4283c2a16233e88d8ea1b661d1783deb0ffd
Tuan Kiet Le <86156449+kademy-software@users.noreply.github.com>
2023-12-13 22:07:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const main_video = document.querySelector('.main-video video');
+const main_video_title = document.querySelector('.main-video .title');
+const video_playlist = document.querySelector('.video-playlist .videos');
+
+let data = [
+    {
+        'id': 'a1',
+        'title': 'Tỉnh',
+        'name': 'dl_tuvung_tinh.mov',
+        'duration': '10 giây',
+    },
+    {
+        'id': 'a2',
+        'title': 'Bắc Bộ',
+        'name': 'dl_tuvung_bacbo.mov',
+        'duration': '4 giây ',
+    },
+    {
+        'id': 'a4',
+        'title': 'Bắc Giang',
+        'name': '',
+        'duration': '7 giây ',
+    },
+    {
+        'id': 'a5',
+        'title': 'Đông Nam Á',
+        'name': 'dna.mp4',
+        'duration': '12 giây',
+    }
+   
+    
+
+
+];
+
+data.forEach((video, i) => {
+    let video_element = `
+                <div class="video" data-id="${video.id}">
+                   
+                    <p>${i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
+                    <h3 class="title">${video.title}</h3>
+                    <p class="time">${video.duration}</p>
+                </div>
+    `;
+    video_playlist.innerHTML += video_element;
+})
+
+let videos = document.querySelectorAll('.video');
+videos[0].classList.add('active');
+
+
+videos.forEach(selected_video => {
+    selected_video.onclick = () => {
+
+        for (all_videos of videos) {
+            all_videos.classList.remove('active');
+            
+        }
+
+        selected_video.classList.add('active');
+        
+
+        let match_video = data.find(video => video.id == selected_video.dataset.id);
+        main_video.src = 'videos/' + match_video.name;
+        main_video_title.innerHTML = match_video.title;
+    }
+});
